aa6826a2a2e95aa316a838369476f729a0c07011
alexcastrom <50254873+alexcastrom@users.noreply.github.com>
2019-06-25 12:51:24 -0400

    Update multi.cpp
    gay

--- multi.cpp
+++ multi.cpp
@@ -1,9 +1,15 @@
+
 #include <iostream>
 #include<stdlib.h>
 #include <cstdlib>
+#include<time.h>
 using namespace std;
 
 int main(){
+
+srand(time(NULL));
+int rand();
+
 
 int c = 0;
 int f = 0;
@@ -20,9 +26,11 @@ cout << "Ingresar numero de columnas de la primera matriz: " ;
 cin >> c;
 for(int i = 0; i < f; i++){
   for (int j = 0; j < c; j++) {
-    int r = (rand() % 100) + 1;
-    array2[i][j] = r;
-    cout << r << endl;
+    //int r = (rand() % 100) + 1;
+
+    int num =1+ rand()%10;
+    array1[i][j] = num;
+   cout << num << endl;
   }
 }
 cout << "Ingresar numero de filas de la segunda matriz: " ;
@@ -33,18 +41,19 @@ if(c == d){
   cout << " Su matriz se puede multiplicar" << endl;
   for(int i = 0; i < d; i++){
     for (int j = 0; j < p; j++) {
-      int t = (rand() % 100) + 1;
-      array2[i][j] = t;
-      cout << t << endl;
+      //int t = (rand() % 100) + 1;
+      int num =1+ rand()%10;
+      array2[i][j] = num;
+     cout << num << endl;
     }
   }
 
   for(int i=0; i<f; i++){
           for(int j=0; j<p; j++){
             array3[i][j] = 0;
-            for(int z=0; z<p; z++){
-                suma = (array1[i][z] * array2[z][j]);
-                array3[i][j] = suma;
+            for(int z=0; z<c; z++){
+              //  suma = (array1[i][z] * array2[z][j]);
+                array3[i][j] = array3[i][j]+ array1[i][z] * array2[z][j];;
                 }
           }
       }
@@ -53,8 +62,8 @@ if(c == d){
       for(int i=0; i<f; ++i){
           for(int j=0; j<p; ++j){
           cout<<array3[i][j]<<" ";
-          cout<<endl;
           }
+          cout<<endl;
       }
 }
 else{
